f19fd30e12d7f9b473d9272ab374577f2a99f114
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2020-03-01 15:01:29 +0300

    Update koefs.cpp

--- koefs.cpp
+++ koefs.cpp
@@ -16,11 +16,11 @@ int main() {
     fin.open(filename);
 
     if (fin.is_open()) {
-        std::unordered_set<std::string> dnf;
+        std::unordered_set<std::string> function_positive_value;
         std::string str;
 
         while (fin >> str) {
-            dnf.insert(str);
+            function_positive_value.insert(str);
             str = "";
         }
 
@@ -48,9 +48,6 @@ int main() {
         }
 
         std::vector<std::unordered_set<int>> unique_set_of_koef{};
-        std::vector<std::vector<int>> vec_of_koef{};
-        unique_set_of_koef.push_back(set_of_koef[0]);
-        vec_of_koef.push_back({0});
         for (int i = 0; i < set_of_koef.size(); ++i) {
             bool is_add = true;
             for (int j = 0; j < unique_set_of_koef.size(); ++j) {
@@ -61,40 +58,32 @@ int main() {
             }
             if (is_add) {
                 unique_set_of_koef.push_back(set_of_koef[i]);
-                std::vector<int> vec;
-                for (auto& j : set_of_koef[i]) {
-                    vec.push_back(j);
-                }
-
-                vec_of_koef.push_back(vec);
             }
         }
 
-        std::vector<std::bitset<6>> integer_function_values;
+        std::vector<std::bitset<6>> integer_function_positive_values;
         for (size_t i = 0; i < 64; ++i) {
-            integer_function_values.emplace_back(std::bitset<6>{i});
+            integer_function_positive_values.emplace_back(std::bitset<6>{i});
         }
 
         std::unordered_set<std::string> set_of_bad_monoms;
 
-        for (auto& function_value : integer_function_values) {
-            if (dnf.find(function_value.to_string()) != dnf.end()) {
+        for (auto& function_value : integer_function_positive_values) {
+            if (function_positive_value.find(function_value.to_string()) != function_positive_value.end()) {
                 continue;
             }
-            for (auto& koef : vec_of_koef) {
+            for (auto& koef : unique_set_of_koef) {
                 std::string str_function_value = function_value.to_string();
                 std::ostringstream out;
 
                 out << '{';
-                for (int i = 0; i < koef.size(); ++i) {
-                    int j = koef[i];
-                    if (str_function_value[5 - j] == '0') {
-                        out << '-' << koef[i];
+                for (auto& i : koef) {
+                    if (str_function_value[5 - i] == '0') {
+                        out << '-' << i;
                     }
                     else {
-                        out << '+' << koef[i];
+                        out << '+' << i;
                     }
-                    out << ",";
                 }
                 out << "}";
 
@@ -102,32 +91,32 @@ int main() {
             }
         }
 
-        for (auto& function_value : integer_function_values) {
-            if (dnf.find(function_value.to_string()) == dnf.end()) {
+        for (auto& function_value : integer_function_positive_values) {
+            if (function_positive_value.find(function_value.to_string()) == function_positive_value.end()) {
                 continue;
             }
+
             std::string str_function_value = function_value.to_string();
-            for (auto& koef : vec_of_koef) {
+            std::string one_equation = {};
+
+            for (auto& koef : unique_set_of_koef) {
                 std::ostringstream out;
                 out << '{';
-                for (int i = 0; i < koef.size(); ++i) {
-                    int j = koef[i];
-                    if (str_function_value[5 - j] == '0') {
-                        out << '-' << koef[i];
+                for (auto &i : koef) {
+                    if (str_function_value[5 - i] == '0') {
+                        out << '-' << i;
+                    } else {
+                        out << '+' << i;
                     }
-                    else {
-                        out << '+' << koef[i];
-                    }
-                    out << ",";
                 }
                 out << "}";
 
                 if (set_of_bad_monoms.find(out.str()) == set_of_bad_monoms.end()) {
-                    std::cout << out.str() << " + ";
+                    one_equation += out.str() + " + ";
                 }
             }
 
-            std::cout << " = " << str_function_value << std::endl;
+            std::cout << std::string(one_equation.begin(), one_equation.end() - 2) << "= " << str_function_value << std::endl;
         }
 
         fin.close();
